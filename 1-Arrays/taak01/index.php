058f31e06f5da4e281d0d6603b078523a9a90c0b
VINCENZO_JOVANOVIC <2151301@talnet.nl>
2022-01-10 15:15:46 +0100

    taak01 af

--- 1-Arrays/taak01/index.php
+++ 1-Arrays/taak01/index.php
@@ -1,1 +1,17 @@
-
+<?php 
+$medewerkers = [
+    [
+        'voornaam' => 'Willem',
+        'achternaam' => 'van Oranje',
+        'functie' => 'Koning'
+    ],
+    [
+        'voornaam' => 'Donald',
+        'achternaam' => 'Trump',
+        'functie' => 'president'
+    ]
+    ];
+echo $medewerkers[0]['voornaam']." ".$medewerkers[0]['achternaam']." "."is een"." ".$medewerkers[0]['functie'];
+echo "<br>";
+echo $medewerkers[1]['voornaam']." ".$medewerkers[1]['achternaam']." "."is een"." ".$medewerkers[1]['functie'];
+?>
